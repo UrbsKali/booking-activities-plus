206b43a2819bacfe21a5dbf2e41f25aea64b4202
Urbain <urbain.lantres@tutanota.com>
2024-05-16 15:57:02 +0200

    remove debug prints

--- js/enable-waiting-list.js
+++ js/enable-waiting-list.js
@@ -6,12 +6,10 @@ $j('.bookacti-booking-form').on('bookacti_trigger_event_click', function (info, 
 
 // connect to bookacti_validate_picked_events with agrs [valid_form]
 $j('.bookacti-booking-form').on('bookacti_validate_picked_events', function (info, valid_form) {
-    console.log('bite');
     console.log(valid_form);
     if (!valid_form.is_qty_inf_to_avail) {
         valid_form.is_qty_inf_to_avail = true;
         valid_form.send = true;
-        console.log('bite1');
     }
 
 });
